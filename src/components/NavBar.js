603b0b3fdea80c93bc27fc6a4c59b9939878637e
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-08-01 20:32:53 +0300

    added navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+
+return (
+    <div>
+        <ul>
+            <li><Link to="/business" >Business</Link></li>
+            <li><Link to="/health" >Health</Link></li>
+            <li><Link to="/entertainment" >Entertainment</Link></li>
+            <li><Link to="/science" >Science</Link></li>
+            <li><Link to="/sports" >Sports</Link></li>
+            <li><Link to="/technology" >Technology</Link></li>
+            <li><Link to="/general" >General</Link></li>
+
+        </ul>
+    </div>
+)
+}
+
+export default Navbar;
