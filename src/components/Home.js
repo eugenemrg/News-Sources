4d8eaaff8dafaaf14d5326f0dd189a6dfcc74ce5
Abogeerick <abogeerick@gmail.com>
2023-08-02 12:45:42 +0300

    Changes in syntax for fetching the articles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Article from "./components/Article";
-import SearchBar from "./components/SearchBar"
+import Articles from "./components/Articles";
+import SearchBar from "./components/SearchBar";
 
+const API_KEY = "607db713f627456d9d081ee0331ce09d";
 
-
-const API_KEY="607db713f627456d9d081ee0331ce09d"
 const Home = () => {
+  const { category } = useParams();
   const [articles, setArticles] = useState([]);
-  const [category, setCategory] = useState(useParams())
 
-  
-   console.log(category.category)
   useEffect(() => {
-  let url= (Object.keys(category).length=== 0) ?
-  `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`:
-  `https://newsapi.org/v2/top-headlines/sources?category=${category.category}&apiKey=${API_KEY}`
+    const fetchArticles = () => {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+      if (category) {
+        url = `https://newsapi.org/v2/top-headlines/sources?category=${category}&apiKey=${API_KEY}`;
+      }
 
-  fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setArticles(data);
-      })
-      .catch((error) => {
-        console.log("Error in fetching the articles", error);
-  })}, []);
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setArticles(data.articles);
+        })
+        .catch((error) => {
+          console.error("Error fetching articles:", error);
+        });
+    };
 
-
+    fetchArticles();
+  }, [category]);
 
   const handleSearch = (searchTerm) => {
     fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${API_KEY}`)
-      .then((res) => res.json())
+      .then((response) => {
+        return response.json();
+      })
       .then((data) => {
-        setArticles(data);
+        setArticles(data.articles);
       })
       .catch((error) => {
-        console.log("Error in fetching the articles", error);
+        console.error("Error fetching articles:", error);
       });
   };
 
   return (
     <div>
       <h1>Latest News</h1>
-      <Articles articles={articles} />         
-      <Search handleSearch={handleSearch} />
+      <Articles articles={articles} />
+      <SearchBar handleSearch={handleSearch} />
     </div>
   );
 }
