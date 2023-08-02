075299250fc5f2436ef9557ab7e61fbef336bd5d
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-02 08:08:32 +0300

    Updated ArticleItem component content

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function ArticleItem() {
+function ArticleItem({article}) {
+
+  const {source, author, title, description, url, urlToImage, publishedAt} = article
+
   return (
-    <div>ArticleItem</div>
+    <div className='article-item'>
+      <div className='item-image'>
+        <img src={urlToImage} alt={title} ></img>
+      </div>
+      <div className='item-details'>
+        <p className='item-title'>{title}</p>
+        <p className='item-description'>{description}</p>
+        <p className='item-authors'>By {author} </p>
+      </div>
+      <button className='item-button'>Read more on {source.name}</button>
+    </div>
   )
 }
 
