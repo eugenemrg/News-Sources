f2f33ed07c1c1e07fe7484e0ed9148cd14713dc2
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-02 11:48:41 +0300

    Updated ArticleItem component, wrapped card in link which goes to news website

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -1,21 +1,23 @@
 import React from 'react'
 
-function ArticleItem({article}) {
+function ArticleItem({ article }) {
 
-  const {source, author, title, description, url, urlToImage, publishedAt} = article
+  const { source, author, title, description, url, urlToImage, publishedAt } = article
 
   return (
     <div className='article-item'>
-      <div className='item-image'>
-        <img src={urlToImage} alt={title} ></img>
-      </div>
-      <div className='item-details'>
-        <p className='item-title'>{title}</p>
-        <p className='item-description'>{description}</p>
-        <p className='item-date'>Published on {publishedAt.split('T')[0]}</p>
-        <p className='item-authors'>By {author ?? 'Anonymous'} </p>
-      </div>
-      <button className='item-button'>Read more on {source.name}</button>
+      <a className='article-item-link' href={url} target='_blank'>
+        <div className='item-image'>
+          <img src={urlToImage} alt={title} ></img>
+        </div>
+        <div className='item-details'>
+          <p className='item-title'>{title}</p>
+          <p className='item-description'>{description}</p>
+          <p className='item-date'>Published on {publishedAt.split('T')[0]}</p>
+          <p className='item-authors'>By {author ?? 'Anonymous'} </p>
+        </div>
+        <button className='item-button'>Read more on {source.name}</button>
+      </a>
     </div>
   )
 }
